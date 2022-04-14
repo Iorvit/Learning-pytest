e11c5bb001660b8427600de1bc81117d8da6ca26
Iorvit <iiorvit@gmail.com>
2022-04-14 12:57:43 +0400

    ch6

--- ch5/pytest-nice/tests/test_nice.py
+++ ch5/pytest-nice/tests/test_nice.py
@@ -9,7 +9,7 @@ def test_pass_fail(testdir):
     testdir.makepyfile("""
         def test_pass():
             assert 1 == 1
-        
+
         def test_fail():
             assert 1 == 2
     """)
@@ -18,7 +18,7 @@ def test_pass_fail(testdir):
     result = testdir.runpytest()
 
     # fnmatch_lines does an assertion internally​”
-    result.stdout.fnmatch_lines(['*.F*',])  # . for Pass, F for Fail
+    result.stdout.fnmatch_lines(['*.F*'])  # . for Pass, F for Fail
 
     # make shure that we get a '1' exit code for the testsuit
     assert result.ret == 1
@@ -29,7 +29,7 @@ def sample_test(testdir):
     testdir.makepyfile("""
         def test_pass():
             assert 1 == 1
-        
+
         def test_fail():
             assert 1 == 2
     """)
@@ -38,30 +38,31 @@ def sample_test(testdir):
 
 def test_with_nice(sample_test):
     result = sample_test.runpytest('--nice')
-    result.stdout.fnmatch_lines(['*.O*',])  # . for Pass, O for Fail
+    result.stdout.fnmatch_lines(['*.O*'])  # . for Pass, O for Fail
     assert result.ret == 1
 
 
 def test_with_verbose_nice(sample_test):
     result = sample_test.runpytest('-v', '--nice')
-    result.stdout.fnmatch_lines(['*::test_fail OPPORTUNITY for improvement*',])
+    result.stdout.fnmatch_lines(['*::test_fail OPPORTUNITY for improvement*'])
     assert result.ret == 1
 
 
 def test_non_nice_verbose(sample_test):
     result = sample_test.runpytest('-v')
-    result.stdout.fnmatch_lines(['*::test_fail FAILED*']) 
+    result.stdout.fnmatch_lines(['*::test_fail FAILED*'])
     assert result.ret == 1
 
 
 def test_header_with_nice(sample_test):
     result = sample_test.runpytest('--nice')
-    result.stdout.fnmatch_lines(['You are breathtaking! Thank you  for running tests.'])
+    result.stdout.fnmatch_lines(['You are breathtaking!'
+                                 ' Thank you  for running tests.'])
 
 
-def test_header_not_nice(sample_test):   
+def test_header_not_nice(sample_test):
     result = sample_test.runpytest()
-    thanks_message = 'You are breathtaking! Thank you  for running tests.'    
+    thanks_message = 'You are breathtaking! Thank you  for running tests.'
     assert thanks_message not in result.stdout.str()
 
 
@@ -71,8 +72,5 @@ def test_help_message(testdir):
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
         'nice:',
-        '*--nice*nice: turn FAILED into OPPORTUNITY for improvement',
-    ])
-
-
-
+        '*--nice*nice: turn FAILED into OPPORTUNITY for improvement']
+    )
